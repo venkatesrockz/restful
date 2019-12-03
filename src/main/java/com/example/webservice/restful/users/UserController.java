bf67629a0ccf8ea68a4371b669ebb6e6c9dbcc1d
Venkatesh Kopula Sivakumar <venkatesrockz@gmail.com>
2019-12-03 12:56:33 +0530

    From STS - First Commit

--- src/main/java/com/example/webservice/restful/users/UserController.java
+++ src/main/java/com/example/webservice/restful/users/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
 
 @RestController
 public class UserController {
